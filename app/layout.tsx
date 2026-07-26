626675eed54c80c5c7e38b6a7212445f4df468be
Djalma Lima <djalmalima@MacBook-Air-de-Djalma.local>
2026-07-26 15:52:38 -0300

    Restore original display fonts for couple names

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
+import "@fontsource-variable/bodoni-moda";
 import "@fontsource-variable/cormorant-garamond";
+import "@fontsource/italiana";
 import "./globals.css";
 
 export const metadata: Metadata = {
